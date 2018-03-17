e653e22f33ec282e73d19306027074cf4e60a9cc
Hon Lui <honlui@gmail.com>
2018-03-17 16:41:24 -0400

    destroy initial test prototype

--- OnlineWhiteBoard_4/OnlineWhiteBoard_4/obj/Debug/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineWhiteBoard_4.MvcApplication" Language="C#" %>
--- SignalRSimple/SignalRSimple/SignalRSimple/SignalR/CounterHub.cs
+++ SignalRSimple/SignalRSimple/SignalRSimple/SignalR/CounterHub.cs
@@ -71,18 +71,18 @@ namespace SignalRSimple.SignalR
         {
             // Create a Long running task to do an infinite loop which will keep sending the server time
             // to the clients every 3 seconds.
-            var taskTimer = Task.Factory.StartNew(async () =>
-            {
-                while (true)
-                {
-                    string timeNow = DateTime.Now.ToString();
-                    //Sending the server time to all the connected clients on the client method SendServerTime()
-                    Clients.All.SendServerTime(timeNow);
-                    //Delaying by 3 seconds.
-                    await Task.Delay(3000);
-                }
-            }, TaskCreationOptions.LongRunning
-                );
+            //var taskTimer = Task.Factory.StartNew(async () =>
+            //{
+            //    while (true)
+            //    {
+            //        string timeNow = DateTime.Now.ToString();
+            //        //Sending the server time to all the connected clients on the client method SendServerTime()
+            //        Clients.All.SendServerTime(timeNow);
+            //        //Delaying by 3 seconds.
+            //        await Task.Delay(3000);
+            //    }
+            //}, TaskCreationOptions.LongRunning
+            //    );
         }
     }
 }
